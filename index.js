6c21e8c124f51a6fbc8a10904c0dca19e81d870e
Mrs  Mac <mrsmac@Mrss-iMac.local>
2019-08-25 19:22:45 +0100

    colors of bricks are white

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function drawBricks() {
             bricks[c][r].y = brickY;
             ctx.beginPath();
             ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
+            ctx.fillStyle = "white";
             ctx.fill();
             ctx.closePath();
         }
